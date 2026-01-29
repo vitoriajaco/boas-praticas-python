79eafe8f5388a4d3d7ff76d3033a831b5ce0aebe
vitoriajaco <vitoriajaco@gmail.com>
2026-01-28 21:27:56 -0300

    learn: entendendo tipos fixos em python

---
+++ case-1.py
@@ -0,0 +1,13 @@
+def add(x: int,y: float) -> float: #Nao sao tipos fixos sao anotacoes aqui vira uma documentacao
+    return x + y
+
+
+
+#Se eu quero que seja obrigatorio mas sem usar bibliotecas 
+def add(x: int, y: float) -> float:
+    if not isinstance(x, int) or not isinstance(y, (int, float)):
+        raise TypeError("Tipos incorretos fornecidos!")
+    return x + y
+
+val = add(2.1, 5)
+print(val)
